0dfe135f2b5cf295414e06d409e71491abfefae3
anirudha mayya <anirudha.mayya@gmail.com>
2021-09-20 23:39:31 +0530

    Added email validation

--- src/main/java/com/testcasedemo/UserRegistration.java
+++ src/main/java/com/testcasedemo/UserRegistration.java
@@ -6,6 +6,11 @@ public class UserRegistration {
 	
 	final String NAME = "^[A-Z][a-z]{2}[a-z]*";
 	final String PHONE = "[1-9][0-9]\\s[1-9][0-9]{9}";
+	
+	final String username = "^[A-Za-z][A-Za-z0-9+-]{2}[A-Za-z0-9+-]*([.][A-Za-z0-9]{2}[A-Za-z0-9]*)?";
+	final String domain = "\\@[a-zA-Z0-9]+\\.[a-z]{2}[a-z]*([.][a-z]{2}[a-z]*)?";
+
+	final String EMAIL = username + domain;
 
 	final String PASSWORD = "(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,20}";
 
@@ -20,6 +25,11 @@ public class UserRegistration {
 
 	public boolean validatePhoneNumber(String phoneNumber) {
 		return Pattern.matches(PHONE, phoneNumber);
+	}
+	
+	public boolean validateEmail(String email) {
+		return Pattern.matches(EMAIL, email);
+
 	}
 	
 	public boolean validatePassword(String password) {
